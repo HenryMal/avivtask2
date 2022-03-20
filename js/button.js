9bdc94e2f94bcff38c9790dbe8176fbed37356b4
Henry Malilay <Henry.J.Malilay-1@ou.edu>
2022-03-20 11:07:51 -0500

    added more constants and fuctions for testing

--- js/button.js
+++ js/button.js
@@ -1,6 +1,17 @@
-const element = document.getElementById("rectangle");
-element.addEventListener("click", myFunction);
+const rect = document.getElementById("rectangle");
+const tri = document.getElementById("triangle");
+const cir = document.getElementById("circle");
+
+rect.addEventListener("click", myFunction);
+tri.addEventListener("click", myFunction2);
+cir.addEventListener("click", myFunction3);
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "Hello World";
+}
+function myFunction2() {
+    document.getElementById("demo1").innerHTML = "Hello World";
+  }
+function myFunction3() {
+    document.getElementById("demo2").innerHTML = "Hello World";
 }
